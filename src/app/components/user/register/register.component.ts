5ca09aecec3d621289bdb009fbb18613771297a5
Imed Attia <attia.imed@beja.r-iset.tn>
2022-08-17 21:47:33 +0100

    commit

--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,10 +11,40 @@ export class RegisterComponent implements OnInit {
     //Path of Register img
     RegisterImgPath:string="../../../../assets/register.jpg";
 
-    
-  constructor() { }
+
+    form : FormGroup = new FormGroup({
+      firstName : new FormControl("",[Validators.required,Validators.email]),
+      lastName: new FormControl("",[Validators.required,Validators.email]),
+      email: new FormControl("",[Validators.required,Validators.email]),
+      phone: new FormControl("",[Validators.required,Validators.email]),
+      password : new FormControl("",[Validators.required,Validators.minLength(10)])
+    })
+    //
+    isSubmited:boolean=false;
 
   ngOnInit(): void {
+  }
+
+  //login fncts
+  Login(){
+    this.isSubmited = true;
+    if(!this.form.invalid){
+      alert('stay')}
+  }
+  get firstName(){
+    return this.form.get("firstName")
+  }
+  get lastName(){
+    return this.form.get("lastName")
+  }
+  get email(){
+    return this.form.get("email")
+  }
+  get phone(){
+    return this.form.get("phone")
+  }
+  get password(){
+    return this.form.get("password")
   }
 
 }
